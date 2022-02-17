cfadb81c8e1265fab0162bf2bdbbce38fea99a1d
Brent Gardner <bgardner@squarelabs.net>
2022-02-17 09:58:53 -0700

    Can play noise

--- src/main.rs
+++ src/main.rs
@@ -42,12 +42,13 @@ async fn msg_loop() -> Result<(), Error> {
     info!("Found {} devices", list.len());
     let dev = list.iter().find(|dev| {
         let desc = dev.device_descriptor().unwrap();
-        desc.vendor_id() == 0x046d && desc.product_id() == 0x0867
+        println!("vid={:#x} pid={:#x}", desc.vendor_id(), desc.product_id());
+        desc.vendor_id() == 0x31b2 && desc.product_id() == 0x10
     }).ok_or(anyhow!("Error finding item!"))?;
     info!("dev={:?}", dev);
     let mut handle = dev.open().unwrap();
     handle.reset().unwrap();
-    handle.unconfigure();
+    // handle.unconfigure();
     let res = handle.set_active_configuration(1);
     info!("set config res={:?}", res);
     if handle.kernel_driver_active(1).unwrap() { handle.detach_kernel_driver(1).unwrap(); }
@@ -57,19 +58,19 @@ async fn msg_loop() -> Result<(), Error> {
     handle.set_alternate_setting(1, 0).unwrap();
     handle.set_alternate_setting(2, 0).unwrap();
 
-    handle.write_control(0x21, 0x0a, 0x0000, 0x03, &[0u8; 0], Duration::from_millis(0)); // set idle
-
-    handle.write_control(0x21, 1, 0x0200, 0x0200, &[0x4cu8, 0xfau8], Duration::from_millis(0)); // set speaker volume
-    handle.write_control(0x21, 1, 0x0200, 0x0600, &[0x60u8, 0xe7u8], Duration::from_millis(0)); // set mic volume
-
-    handle.write_control(0x21, 0x09, 0x0231, 0x03, &[0x31u8, 0x00u8], Duration::from_millis(0)); // set report
-    handle.write_control(0x21, 0x09, 0x0231, 0x03, &[0x31u8, 0x00u8], Duration::from_millis(0)); // set report
-    handle.write_control(0x21, 0x09, 0x0231, 0x03, &[0x31u8, 0x00u8], Duration::from_millis(0)); // set report
-    handle.write_control(0x21, 0x09, 0x0231, 0x03, &[0x31u8, 0x00u8], Duration::from_millis(0)); // set report
-    handle.write_control(0x21, 0x09, 0x0231, 0x03, &[0x31u8, 0x00u8], Duration::from_millis(0)); // set report
+    // handle.write_control(0x21, 0x0a, 0x0000, 0x03, &[0u8; 0], Duration::from_millis(0)); // set idle
+    //
+    // handle.write_control(0x21, 1, 0x0200, 0x0200, &[0x4cu8, 0xfau8], Duration::from_millis(0)); // set speaker volume
+    // handle.write_control(0x21, 1, 0x0200, 0x0600, &[0x60u8, 0xe7u8], Duration::from_millis(0)); // set mic volume
+    //
+    // handle.write_control(0x21, 0x09, 0x0231, 0x03, &[0x31u8, 0x00u8], Duration::from_millis(0)); // set report
+    // handle.write_control(0x21, 0x09, 0x0231, 0x03, &[0x31u8, 0x00u8], Duration::from_millis(0)); // set report
+    // handle.write_control(0x21, 0x09, 0x0231, 0x03, &[0x31u8, 0x00u8], Duration::from_millis(0)); // set report
+    // handle.write_control(0x21, 0x09, 0x0231, 0x03, &[0x31u8, 0x00u8], Duration::from_millis(0)); // set report
+    // handle.write_control(0x21, 0x09, 0x0231, 0x03, &[0x31u8, 0x00u8], Duration::from_millis(0)); // set report
 
     // thread::sleep(Duration::from_millis(1000));
-    handle.set_alternate_setting(2, 1).unwrap();
+    handle.set_alternate_setting(2, 2).unwrap();
     thread::sleep(Duration::from_millis(250));
 
     let ctx = GlobalContext::default();;
