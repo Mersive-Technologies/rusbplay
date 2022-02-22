5fed044f47e7787f232b94be401a9439a0262f44
Brent <bgardner@squarelabs.net>
2022-02-22 09:06:21 -0700

    Can find device

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,24 @@ extern crate log;
 #[macro_use]
 extern crate anyhow;
 
-use anyhow::Error;
-use rusb::{DeviceList, GlobalContext, DeviceHandle, UsbContext};
-use futures::executor;
-use futures::future::SelectAll;
-use std::thread;
-use std::time::Duration;
+use anyhow::{Error};
+use rusb::{DeviceList};
 
-fn main() {
+fn main() -> Result<(), Error> {
     println!("Hello, world!");
+
+    let list = DeviceList::new()?;
+    info!("Found {} devices", list.len());
+    let dev = list.iter().find(|dev| {
+        match dev.device_descriptor() {
+            Ok(desc) => {
+                desc.vendor_id() == 0x0bda && desc.product_id() == 0x48a8
+            },
+            _ => false
+        }
+    }).ok_or(anyhow!("Error finding item!"))?;
+    println!("dev={:?}", dev);
+
+    Ok(())
 }
 
