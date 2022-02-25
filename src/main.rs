68703ccce77e224dc730349e2be9f21b54725bcc
Brent <bgardner@squarelabs.net>
2022-02-25 09:08:00 -0700

    [WIP] config

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ extern crate anyhow;
 use std::ffi::c_void;
 use std::os::raw::c_uchar;
 use std::ptr::null_mut;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
 use anyhow::{Context, Error};
 use futures::channel::mpsc::{channel, Receiver, Sender};
@@ -28,6 +26,18 @@ pub struct TransferContext {
     result_tail: Sender<TransferResult>,
 }
 
+pub struct Config {
+    pub vid: u16, // VendorID of USB digital-analog converter
+    pub pid: u16, // ProductID of USB digital-analog converter
+    pub iface: u8, // Speaker interface (as opposed to mic)
+    pub ep: c_uchar, // Endpoint 4 - speaker
+    pub set_enabled: u8, // 48khz (see descriptors in readme)
+    pub set_disable: u8, // Standard
+    pub pkt_sz: usize, // 48000hz * 16bits * 2chan = 192,000byte/sec / 192 = 1ms of audio
+    pub pkt_cnt: usize, // Each transfer contains 10ms of audio
+    pub buff_cnt: i32, // Number of buffers in ring
+}
+
 fn main() -> Result<(), Error> {
     println!("Hello, world!");
 
@@ -41,33 +51,35 @@ fn main() -> Result<(), Error> {
 
 unsafe fn run() -> Result<(), Error> {
     // do math
-    let vid = 0x0bda; // VendorID of USB digital-analog converter
-    let pid = 0x48a8; // ProductID of USB digital-analog converter
-    let iface = 2; // Speaker interface (as opposed to mic)
-    let ep = 4; // Endpoint 4 - speaker
-    let set_enabled = 2; // 48khz (see descriptors in readme)
-    let set_disable = 0; // Standard
-    let pkt_sz = 192; // 48000hz * 16bits * 2chan = 192,000byte/sec / 192 = 1ms of audio
-    let pkt_cnt = 10; // Each transfer contains 10ms of audio
-    let buff_cnt = 2; // Number of buffers in ring
+    let cfg = Config {
+        vid: 0x0bda,
+        pid: 0x48a8,
+        iface: 2,
+        ep: 4,
+        set_enabled: 2,
+        set_disable: 0,
+        pkt_sz: 192,
+        pkt_cnt: 10,
+        buff_cnt: 2,
+    };
 
     // Find and open device
     let list = DeviceList::new()?;
     info!("Found {} devices", list.len());
     let dev = list.iter().find(|dev| {
         match dev.device_descriptor() {
-            Ok(desc) => desc.vendor_id() == vid && desc.product_id() == pid,
+            Ok(desc) => desc.vendor_id() == cfg.vid && desc.product_id() == cfg.pid,
             _ => false
         }
     }).ok_or(anyhow!("Error finding item!"))?;
     println!("dev={:?}", dev);
     let mut handle = dev.open().context("Error opening device!")?;
-    handle.claim_interface(iface).unwrap();
+    handle.claim_interface(cfg.iface).unwrap();
     
     // allocate transfer
-    let mut buffers: Vec<Vec<i16>> = (0..buff_cnt).map(|_| vec![0i16; pkt_cnt * pkt_sz / 2]).collect();
+    let mut buffers: Vec<Vec<i16>> = (0..cfg.buff_cnt).map(|_| vec![0i16; cfg.pkt_cnt * cfg.pkt_sz / 2]).collect();
     let (xfers, errors): (Vec<_>, Vec<_>) = buffers.iter().map(|mut b|
-        alloc_xfer(ep, pkt_sz, pkt_cnt, &mut handle, &mut b)
+        alloc_xfer(&cfg, &mut handle, &mut b)
     ).partition(Result::is_ok);
     if let Some(e) = errors.first() {
         e.context("Error allocating transfer")?;
@@ -76,15 +88,16 @@ unsafe fn run() -> Result<(), Error> {
 
     let (result_tail, result_head): (Sender<TransferResult>, Receiver<TransferResult>) = channel(0);
 
-    for xfer in &xfers {
-        submit(iface, set_enabled, set_disable, &mut handle, &result_tail)?;
+    let mut samp_idx = 0;
+    for (idx, xfer) in xfers.iter().enumerate() {
+        let mut buffer = &mut buffers[idx];
+        fill_buff(buffer, &mut samp_idx);
+        submit(&cfg, idx, &mut handle, &result_tail)?;
     }
 
-    let mut samp_idx = 0;
     loop {
-        fill_buff(&mut buffer, &mut samp_idx);
 
-        submit(iface, set_enabled, set_disable, &mut handle, &result_tail);
+        submit(&cfg, &mut handle, &result_tail);
 
         let timeout = Duration::from_millis(100);
         loop {
@@ -98,7 +111,7 @@ unsafe fn run() -> Result<(), Error> {
     }
 }
 
-unsafe fn submit(iface: u8, set_enabled: u8, set_disable: u8, idx: usize,
+unsafe fn submit(cfg: &Config, idx: usize,
                  handle: &mut DeviceHandle<GlobalContext>, result_tail: &Sender<TransferResult>
 ) -> Result<(), Error> {
     for _ in 0..2 {
@@ -112,8 +125,8 @@ unsafe fn submit(iface: u8, set_enabled: u8, set_disable: u8, idx: usize,
             println!("Transfer submitted {}", res);
             break;
         }
-        handle.set_alternate_setting(iface, set_disable).unwrap();
-        handle.set_alternate_setting(iface, set_enabled).unwrap();
+        handle.set_alternate_setting(cfg.iface, cfg.set_disable).unwrap();
+        handle.set_alternate_setting(cfg.iface, cfg.set_enabled).unwrap();
     }
     Err(anyhow!("Failed to submit!"))
 }
@@ -133,29 +146,29 @@ unsafe fn fill_buff(buffer: &mut Vec<i16>, samp_idx: &mut usize) {
     (*samp_idx) += buffer.len();
 }
 
-unsafe fn alloc_xfer(ep: c_uchar, pkt_sz: usize, pkt_cnt: usize,
+unsafe fn alloc_xfer(cfg: &Config,
                      handle: &mut DeviceHandle<GlobalContext>,
                      buffer: &mut Vec<i16>
 ) -> Result<*mut libusb_transfer, Error> {
-    let sz = pkt_cnt * pkt_sz;
-    let mut xfer = *&libusb_alloc_transfer(pkt_cnt as i32);
+    let sz = cfg.pkt_cnt * cfg.pkt_sz;
+    let mut xfer = *&libusb_alloc_transfer(cfg.pkt_cnt as i32);
     if xfer == null_mut() {
         Err(anyhow!("libusb_alloc_transfer failed!"))?;
     }
     (*xfer).dev_handle = handle.as_raw();
-    (*xfer).endpoint = ep;
+    (*xfer).endpoint = cfg.ep;
     (*xfer).transfer_type = LIBUSB_TRANSFER_TYPE_ISOCHRONOUS; // reserve seats on the bus
     (*xfer).timeout = 0;
-    (*xfer).num_iso_packets = pkt_cnt as i32;
+    (*xfer).num_iso_packets = cfg.pkt_cnt as i32;
     (*xfer).callback = *&iso_complete_handler;
     (*xfer).length = sz as i32;
     (*xfer).buffer = buffer.as_mut_ptr() as *mut u8;
 
     // Fill in packet descriptors
     let pkt_descs = (*xfer).iso_packet_desc.as_mut_ptr();
-    for i in 0usize..pkt_cnt {
+    for i in 0usize..cfg.pkt_cnt {
         let pkt_desc = pkt_descs.add(i);
-        (*pkt_desc).length = pkt_sz as u32;
+        (*pkt_desc).length = cfg.pkt_sz as u32;
         (*pkt_desc).actual_length = 0;
         (*pkt_desc).status = 0;
     }
